3d07ecdfc231c8addbae7fca05180d05b2763769
Diego Duran <diego.duran.rguez@gmail.com>
2019-08-31 23:49:21 +0200

    19. Callbacks

---
+++ 02-fundamentos/callback.js
@@ -0,0 +1,25 @@
+/*jshint esversion: 6 */
+// setTimeout(() => {
+//     console.log('Hola Mundo');
+// }, 3000);
+
+let getUsuarioById = (id, callback) => {
+    let usuario = {
+        nombre: 'Fernando',
+        id // Es reundante, por lo que id: id es lo mismo que id
+    };
+
+    if (id === 20) {
+        callback(`El usuario con la id ${ id } no existe en la BD`);
+    } else {
+        callback(null, usuario);
+    }
+};
+
+getUsuarioById(10, (err, usuario) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Usuario de base de datos', usuario);
+});
