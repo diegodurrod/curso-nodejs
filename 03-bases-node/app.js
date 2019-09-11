d29c17708f6ccb28304c98b66dfc8e7380ab6bf1
Diego Duran <diego.duran.rguez@gmail.com>
2019-09-11 16:29:03 +0200

    31. Recibir informacion desde linea de comando

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -3,11 +3,17 @@
 //const multiplicar = require('./multiplicar/multiplicar');
 const { crearArchivo } = require('./multiplicar/multiplicar'); // utilizando destructuracion de objetos
 
-let base = 7;
-
 // Sin la destructuracion de objetos
 //console.log(multiplicar);
 //multiplicar.crearArchivo
+
+// console.log(process.argv);
+let argv = process.argv;
+let parametro = argv[2];
+
+let base = parametro.split('=')[1];
+
+console.log(base);
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${ archivo }`))
