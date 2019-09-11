e602a54356ee643332b585e71edb889d3fece49f
Diego Duran <diego.duran.rguez@gmail.com>
2019-09-11 16:55:27 +0200

    33. Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,5 +1,19 @@
 /*jshint esversion: 2017 */
 
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+
 //const multiplicar = require('./multiplicar/multiplicar');
 const { crearArchivo } = require('./multiplicar/multiplicar'); // utilizando destructuracion de objetos
 
@@ -8,13 +22,14 @@ const { crearArchivo } = require('./multiplicar/multiplicar'); // utilizando des
 //multiplicar.crearArchivo
 
 // console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
+let argv2 = process.argv;
 
-let base = parametro.split('=')[1];
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
 
-console.log(base);
+console.log(argv.limite);
+// console.log(argv2);
 
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${ archivo }`))
-    .catch(e => console.log(e));
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+//     .catch(e => console.log(e));
