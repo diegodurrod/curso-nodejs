b4a710631190d22bb711505520f870f50895867f
Diego Duran <diego.duran.rguez@gmail.com>
2019-09-20 01:44:40 +0200

    Inicio del proyecto - Webserver

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -10,13 +10,26 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-lugar.getLugarLatLng(argv.direccion)
-    .then((result) => {
-        console.log(result);
-    }).catch((err) => {
-        console.log('ERROR!!', err);
-    });
+// lugar.getLugarLatLng(argv.direccion)
+//     .then((result) => {
+//         console.log(result);
+//     }).catch((err) => {
+//         console.log('ERROR!!', err);
+//     });
 
-clima.getClima(40.419998, -3.700000)
-    .then(console.log)
-    .catch(console.log);
+// clima.getClima(40.419998, -3.700000)
+//     .then(console.log)
+//     .catch(console.log);
+
+const getInfo = async(direccion) => {
+    try {
+        const coordenadas = await lugar.getLugarLatLng(direccion);
+        const tiempo = await clima.getClima(coordenadas.lat, coordenadas.lng);
+
+        console.log(`El clima de ${ direccion } es de ${ tiempo }`);
+    } catch (error) {
+        throw new Error(`No se pudo determinar el clima de ${ direccion }`, error);
+    }
+};
+
+getInfo(argv.direccion);
