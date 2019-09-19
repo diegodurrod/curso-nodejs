cf38c9a46fc006d8e3947e784762006603b563de
Diego Duran <diego.duran.rguez@gmail.com>
2019-09-20 00:12:25 +0200

    56. Peticiones HTTP-Axios-Request

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 2017 */
+const axios = require('axios');
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -8,3 +9,19 @@ const argv = require('yargs').options({
 }).argv;
 
 console.log(argv.direccion);
+const encodeURL = encodeURI(argv.direccion);
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeURL }`,
+    headers: {
+        'x-rapidapi-host': 'devru-latitude-longitude-find-v1.p.rapidapi.com',
+        'x-rapidapi-key': '9956769caemshd54829e18324610p1fcad7jsn3db3c9ad659c'
+    }
+});
+
+instance.get()
+    .then((resp) => {
+        console.log(resp.data.Results[0]);
+    }).catch((err) => {
+        console.log('ERROR!!!!', err);
+    });
