81de6d764baa94d474b41c63f4698748067bc9c6
Diego Duran <diego.duran.rguez@gmail.com>
2019-09-21 01:08:05 +0200

    66. Servir contenido estatico

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -2,20 +2,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    let salida = {
-        nombre: 'Diego',
-        edad: 28,
-        url: req.url
-    };
-    res.send(salida);
+// De esta forma se podra servir lo que esta en la carpeta public
+// index.html, home.html...
+app.use(express.static(__dirname + '/public'));
 
-    // res.send('Hola Mundo');
-});
+// app.get('/', (req, res) => {
+//     let salida = {
+//         nombre: 'Diego',
+//         edad: 28,
+//         url: req.url
+//     };
+//     res.send(salida);
 
-app.get('/data', (req, res) => {
-    res.send('Hola Data');
-});
+//     // res.send('Hola Mundo');
+// });
+
+// app.get('/data', (req, res) => {
+//     res.send('Hola Data');
+// });
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
