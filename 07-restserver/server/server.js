5e087190d5e32c98f1f8b7613cf6cd71383b0195
Diego Duran <diego.duran.rguez@gmail.com>
2019-09-24 23:14:43 +0200

    80. Peticiones HTTP - GET - PUT - POST - DELETE

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,10 +1,39 @@
 /*jshint esversion: 2017 */
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.json('Hello World');
-})
+});
+
+app.get('/usuarios', function(req, res) {
+    res.json('Get Usuarios');
+});
+
+app.post('/usuarios', function(req, res) {
+    let body = req.body;
+    res.json({
+        persona: body
+    });
+});
+
+app.put('/usuarios/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuarios', function(req, res) {
+    res.json('Delete Usuarios');
+});
 
 app.listen(3000, () => {
     console.log('Escuchando el puerto 3000');
