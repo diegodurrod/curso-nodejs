dd84a435491945635132d4bb5f8536103daf6531
Diego Duran <diego.duran.rguez@gmail.com>
2019-10-06 19:11:54 +0200

    103. Delete: Marcar una eliminacion en el mismo registro

--- 07-restserver/server/routes/usuario.js
+++ 07-restserver/server/routes/usuario.js
@@ -39,10 +39,10 @@ app.get('/usuarios', function(req, res) {
                 });
             });
 
-            res.json({
-                ok: true,
-                usuarios
-            });
+            // res.json({
+            //     ok: true,
+            //     usuarios
+            // });
         });
 });
 
@@ -114,8 +114,31 @@ app.put('/usuarios/:id', function(req, res) {
     // });
 });
 
-app.delete('/usuarios', function(req, res) {
-    res.json('Delete Usuarios');
+app.delete('/usuarios/:id', function(req, res) {
+    let id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
 });
 
 module.exports = app;
