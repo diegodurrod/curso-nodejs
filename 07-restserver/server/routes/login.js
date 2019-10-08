c9dd599b12592c27a13bcb5d34ffbe797d8daf62
Diego Duran <diego.duran.rguez@gmail.com>
2019-10-08 23:23:04 +0200

    121. Login de usuario

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -6,9 +6,45 @@ const bcrypt = require('bcrypt');
 const app = express();
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true
+    let body = req.body;
+
+    // La primera condicion es para establecer una condicion de busqueda
+    // La segunda condicion es para establecer un callback de ejecucion de la consulta
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        // Si no existe el usuario
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Usuario o contrasena incorrectos"
+                }
+            });
+        }
+
+        // Si la contrasena no coincide con la encriptada en la base de datos
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Usuario o contrasena incorrectos"
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token: '123'
+        });
     });
+
 });
 
 module.exports = app;
