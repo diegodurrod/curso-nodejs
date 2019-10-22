6826b502eebe204633019043e42aea18568d166d
Diego Duran <diego.duran.rguez@gmail.com>
2019-10-22 22:37:43 +0200

    156. Validar la extension del archivo a subir

--- 07-restserver/server/routes/upload.js
+++ 07-restserver/server/routes/upload.js
@@ -17,8 +17,22 @@ app.put('/upload', (req, res) => {
     }
 
     let archivo = req.files.archivo;
+    let nombreCortado = archivo.name.split('.');
+    let extension = nombreCortado[nombreCortado.length - 1];
 
-    archivo.mv('uploads/filename.jpg', (err) => {
+    // Extensiones permitidas
+    let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+
+    if (extensionesValidas.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Las extensiones permitidas son ' + extensionesValidas.join(', ')
+            }
+        });
+    }
+
+    archivo.mv(`uploads/${ archivo.name }`, (err) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
