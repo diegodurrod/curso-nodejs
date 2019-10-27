0135233e8df90242a4b5a1405899d175da8a4a04
Diego Duran <diego.duran.rguez@gmail.com>
2019-10-27 23:38:01 +0100

    162. Mostrar imagen de usuario o producto

--- 07-restserver/server/routes/imagenes.js
+++ 07-restserver/server/routes/imagenes.js
@@ -3,17 +3,22 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const { verificaToken } = require('../middlewares/autenticacion');
+
 let app = express();
 
-app.get('/imagen/:tipo/:img', (req, res) => {
+app.get('/imagen/:tipo/:img', verificaToken, (req, res) => {
     let tipo = req.params.tipo;
     let img = req.params.img;
 
-    let pathImg = `./uploads/${ tipo }/${ img }`;
-
+    let pathImagen = path.resolve(__dirname, `../../uploads/${ tipo }/${ img }`);
     let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
 
-    res.sendFile(noImagePath);
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    } else {
+        res.sendFile(noImagePath);
+    }
 });
 
 module.exports = app;
