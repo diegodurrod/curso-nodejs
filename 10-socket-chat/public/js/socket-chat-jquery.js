68abbabda1d2f346fef21d2b0c25b1f7fad8d42b
Diego Duran <diego.duran.rguez@gmail.com>
2019-11-05 10:28:52 +0100

    210 Obtener el ID del usuario conectado

--- 10-socket-chat/public/js/socket-chat-jquery.js
+++ 10-socket-chat/public/js/socket-chat-jquery.js
@@ -21,3 +21,11 @@ function renderizarUsuarios(personas) {
 
     divUsuarios.html(html);
 }
+
+// Listeners
+divUsuarios.on('click', 'a', function() {
+    var id = $(this).data('id');
+    if(id) {
+        console.log(id);
+    }
+});
